ed89b7aec2ffd41efac07d5d6ef8fb82c1132583
Nick Mukhin <mukhin.nick@gmail.com>
2014-11-13 07:25:51 +0200

    database content

---
+++ AmSaf/src/java/com/as/session/PoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.as.session;
+
+import com.as.Po;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author nick
+ */
+@Stateless
+public class PoFacade extends AbstractFacade<Po> {
+    @PersistenceContext(unitName = "AmSafPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PoFacade() {
+        super(Po.class);
+    }
+    
+}
---
+++ AmSaf/src/java/com/as/session/QuoteFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.as.session;
+
+import com.as.Quote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author nick
+ */
+@Stateless
+public class QuoteFacade extends AbstractFacade<Quote> {
+    @PersistenceContext(unitName = "AmSafPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public QuoteFacade() {
+        super(Quote.class);
+    }
+    
+}
